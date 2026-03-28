f1849d49e7f4ffea610eca20860c64298f8efda1
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2026-03-28 21:15:03 +0530

    Setting up processing worker

---
+++ processing-worker/src/main/java/com/telemetry/processing/ProcessingWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.telemetry.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingWorkerApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ProcessingWorkerApplication.class, args);
+    }
+
+}
